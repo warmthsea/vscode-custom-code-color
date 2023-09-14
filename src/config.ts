2dcc6cc6624885ad8ab4b2038e1b8f586da2b633
warmthsea <2586244885@qq.com>
2023-09-14 10:44:07 +0800

    feat: core

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+/* IMPORT */
+
+import * as vscode from 'vscode'
+
+/* CONFIG */
+
+const Config = {
+
+  get(extension = 'vscodeCustomCodeColor') {
+    return vscode.workspace.getConfiguration().get(extension) as any
+  },
+
+}
+
+/* EXPORT */
+
+export default Config
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,44 @@
-import { window } from 'vscode'
+import type { ExtensionContext, TextEditorDecorationType } from 'vscode'
+import * as vscode from 'vscode'
 
-export function activate() {
-  window.showInformationMessage('Hello')
+export function activate(context: ExtensionContext) {
+  vscode.window.showInformationMessage('Hello')
+
+  const decorationType = vscode.window.createTextEditorDecorationType({
+    color: '#257F80', // 设置颜色
+  })
+
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+      if (editor && isVueFile(editor))
+        updateDecorations(editor, decorationType)
+    }),
+    vscode.workspace.onDidChangeTextDocument((event) => {
+      if (vscode.window.activeTextEditor && isVueFile(vscode.window.activeTextEditor) && event.document === vscode.window.activeTextEditor.document)
+        updateDecorations(vscode.window.activeTextEditor, decorationType)
+    }),
+  )
 }
 
-export function deactivate() {
-
+function isVueFile(editor: any) {
+  return editor.document.languageId === 'vue'
 }
+
+function updateDecorations(editor: any, decorationType: TextEditorDecorationType) {
+  const text = editor.document.getText()
+  const regex = /v-role/g
+
+  const ranges = []
+  let match
+  // eslint-disable-next-line no-cond-assign
+  while ((match = regex.exec(text))) {
+    const startPos = editor.document.positionAt(match.index)
+    const endPos = editor.document.positionAt(match.index + match[0].length)
+    const range = new vscode.Range(startPos, endPos)
+    ranges.push(range)
+  }
+
+  editor.setDecorations(decorationType, ranges)
+}
+
+export function deactivate() {}
